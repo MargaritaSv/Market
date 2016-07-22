9955f65917a8bab83bba5439c00427f52e1be690
User-11 <user-11@Trainings-Mac-mini-9.local>
2016-07-22 15:48:09 +0300

    Create class Dealer with fields and constructor and class with main method.

---
+++ Dealer.java
@@ -0,0 +1,33 @@
+package MarketTask;
+
+public class Dealer {
+
+	private String name;
+	private String addres;
+	private double capital;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddres() {
+		return addres;
+	}
+
+	public void setAddres(String addres) {
+		this.addres = addres;
+	}
+
+	public double getCapital() {
+		return capital;
+	}
+
+	public void setCapital(double capital) {
+		this.capital = capital;
+	}
+	
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+package MarketTask;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
